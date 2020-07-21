76ddf28f20d3552209dbfdc4fae9e4791929a637
t.mizuno <t.mizuno150@gmail.com>
2020-07-21 22:43:26 +0900

    Header componentのスタイル調整

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,9 +1,22 @@
 import React, {FC} from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
+
+import Style from '../../assets/scss/components/Header.module.scss'
 
 const Header: FC = () => (
-  <div>
-    ヘッダーだお
-  </div>
+  <header className={Style.header}>
+    <div className={Style.item}>
+      <Link to="/">Youtube-clone</Link>
+    </div>
+    <div className={Style.item}>
+      <form>
+        <input type="text" placeholder="search"/>
+        <button type="submit"><FontAwesomeIcon icon={faSearch} /></button>
+      </form>
+    </div>
+  </header>
 )
 
 export default Header
